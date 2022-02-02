83a258b295aec12b55e901c8d15cad1a15efc8f8
Michal <m.urbanek@wasko.pl>
2022-02-02 10:51:06 +0100

    Added task model and task repository

---
+++ src/main/java/pl/wasko/todoapp/model/Task.java
@@ -0,0 +1,23 @@
+package pl.wasko.todoapp.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String description;
+    private boolean done;
+
+
+
+}
---
+++ src/main/java/pl/wasko/todoapp/model/TaskRepository.java
@@ -0,0 +1,8 @@
+package pl.wasko.todoapp.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface TaskRepository extends JpaRepository<Task,Integer> {
+}
