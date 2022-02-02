801cba71b8689e8a4495d8500b850d0233d7afd4
Michal <m.urbanek@wasko.pl>
2022-02-02 15:02:05 +0100

    Added pagination

--- src/main/java/pl/wasko/todoapp/controller/TaskController.java
+++ src/main/java/pl/wasko/todoapp/controller/TaskController.java
@@ -3,12 +3,17 @@ package pl.wasko.todoapp.controller;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import pl.wasko.todoapp.model.Task;
 import pl.wasko.todoapp.model.TaskRepository;
+
+import java.util.List;
+
 
 @AllArgsConstructor
 @RepositoryRestController
@@ -17,8 +22,14 @@ public class TaskController {
     private final TaskRepository taskRepository;
 
     @GetMapping(value = "/tasks",params = {"!sort","!page","!size"})
-    ResponseEntity<?> readAllTasks(){
+    ResponseEntity<List<Task>> readAllTasks(){
         logger.warn("Exposing all the tasks!");
         return ResponseEntity.ok(taskRepository.findAll());
+    }
+
+    @GetMapping(value = "/tasks")
+    ResponseEntity<?> readAllTasks(Pageable page){
+        logger.warn("Custom pageable");
+        return ResponseEntity.ok(taskRepository.findAll(page).getContent());
     }
 }
