99a24d718f512dfc406af6240e2e8c1a8671daf7
Michal <m.urbanek@wasko.pl>
2022-02-04 07:52:50 +0100

    Flyway SQL and Java Migration, added custom select

--- src/main/java/pl/wasko/todoapp/model/SqlTaskRepository.java
+++ src/main/java/pl/wasko/todoapp/model/SqlTaskRepository.java
@@ -1,6 +1,7 @@
 package pl.wasko.todoapp.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
@@ -10,5 +11,7 @@ import java.util.List;
 
 @Repository()
  interface SqlTaskRepository extends TaskRepository,JpaRepository<Task,Integer> {
-
+ @Override
+ @Query(nativeQuery = true,value = "select count(*)>0 from tasks where id=:id")
+ boolean existsById(@Param("id") Integer id);
 }
--- src/main/resources/db/migration/V1__init_tasks_table.sql
+++ src/main/resources/db/migration/V1__init_tasks_table.sql
@@ -1,6 +1,6 @@
 drop table if exists tasks;
 create table tasks(
-    int int primary key auto_increment,
+    id int primary key auto_increment,
     description varchar(100) not null,
     done bit
 )
